8ba1265e6dd60c4ec8791a92fad63998f18506ec
simran2195 <simran13104@iiitd.ac.in>
2023-03-13 02:47:06 -0700

    updated csv reading sensor data

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, render_template
 import matplotlib.pyplot as plt
 from io import BytesIO
 import os
+import pandas as pd
+import numpy as np
 
 
 plt.switch_backend('Agg')
@@ -18,9 +20,29 @@ def root():
 @app.route('/visualization/<int:id>')
 def generate_visualization(id):
     # Generate the image using Matplotlib
+    # fig, ax = plt.subplots()
+    # ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+    # ax.set_title('Sample Visualization')
+    # img_bytes = BytesIO()
+    # fig.savefig(img_bytes, format='png')
+    # plt.close(fig)
+    # img_bytes.seek(0)
+
+        # Read the CSV file
+    data = pd.read_csv('static/data.csv')
+    z_acc = data["Linear Acceleration z (m/s^2)"]
+    t = np.linspace(0, 10, len(z_acc), endpoint=False)
+
+
+    # Generate the plot using Matplotlib
     fig, ax = plt.subplots()
-    ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
-    ax.set_title('Sample Visualization')
+    # ax.plot(data['x'], data['y'])
+    ax.plot(t, z_acc)
+    ax.set_xlabel('Time [samples]')
+    ax.set_ylabel('Z-Axis Acceleration (m/s^2)')
+    # ax.ylabel("Z-Axis Acceleration (m/s^2)")
+    # ax.xlabel("Time [samples]")
+    ax.set_title('Z-axis accelerometer raw data')
     img_bytes = BytesIO()
     fig.savefig(img_bytes, format='png')
     plt.close(fig)
